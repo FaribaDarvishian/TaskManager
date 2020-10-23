ae59a7ab8ad24673ec709bcf57c71d61dc968835
fariba <fariba.darvishyan1994@gmail.com>
2020-10-23 16:04:33 +0330

    add User in model Package

--- app/src/main/java/com/example/taskmanager/controller/activity/SignUpActivity.java
+++ app/src/main/java/com/example/taskmanager/controller/activity/SignUpActivity.java
@@ -1,15 +1,25 @@
 package com.example.taskmanager.controller.activity;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.taskmanager.R;
+import com.example.taskmanager.controller.fragment.SignUpFragment;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends SingleFragmentActivity {
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, SignUpActivity.class);
+        return intent;
+    }
 
+    @Override
+    public Fragment createFragment() {
+        SignUpFragment signUpFragment = SignUpFragment.newInstance();
+        return signUpFragment;
+    }
 
 }
